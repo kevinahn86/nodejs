9e79374a08c7b87410bfce6021d3335790e0a202
kevinahn86 <snowangel86@daum.net>
2020-04-27 14:02:50 +0900

    update

--- main.js
+++ main.js
@@ -114,21 +114,41 @@ var app = http.createServer(function(request,response){
         var title = queryData.id;
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
           var template= templateHTML(title,list,
-            `<form action="http://localhost:3000/create_process"
+            `<form action="http://localhost:3000/update_process"
                   method="post">
+              <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" value="${title}"></p>
               <p>
-                <textarea name="description" placeholder="description"></textarea>
+                <textarea name="description" placeholder="description">${description}</textarea>
               </p>
               <p>
                 <input type="submit">
               </p>
-            </form>`, `${queryData.id}`);
+            </form>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         });
       });
 
+    }else if(pathname ==='/update_process'){
+
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            });
+          });
+      });
+      
     }
 
     else{
