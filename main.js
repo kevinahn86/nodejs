72c7734517482f5d1bd030f05ec713ec93d594e0
kevinahn86 <snowangel86@daum.net>
2020-04-29 13:42:49 +0900

    mysql

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host:'localhost',
+  user:'ahn',
+  password:'dkswlxkr',
+  database:'ahn'
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
 
@@ -16,41 +24,38 @@ var app = http.createServer(function(request,response){
 
       if(queryData.id === undefined){
 
-        fs.readdir('./data', function(err,filelist){
+        db.query(`select * from member`,function(err, result){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-
-          var list = template.list(filelist);
-          var html= template.html(title,list,
-            `<h2>${title}</h2>${description}`
-            ,`<a href="/create">create</a>`);
+          var list = template.list(result);
+          var html = template.html(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`)
           response.writeHead(200);
           response.end(html);
         });
-
 
       }else{
 
-        fs.readdir('./data', function(err,filelist){
-          var list = template.list(filelist);
-          var title = queryData.id;
-          var filteredID = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredID}`, 'utf8', function(err,description){
-          var html= template.html(title,list,
-            `<h2>${title}</h2>${description}`
-          ,`<a href="/create">create</a>
-          <a href="/update?id=${title}">update</a>
-          <form action="delete_process"
-                method="post">
-            <input type="hidden" name="id" value="${title}">
-            <input type="submit" value="delete">
-          </form>
-          `);
-          response.writeHead(200);
-          response.end(html);
-        });
+        // fs.readdir('./data', function(err,filelist){
+        //   var list = template.list(filelist);
+        //   var title = queryData.id;
+        //   var filteredID = path.parse(queryData.id).base;
 
-        });
+        // fs.readFile(`data/${filteredID}`, 'utf8', function(err,description){
+        //   var html= template.html(title,list,
+        //     `<h2>${title}</h2>${description}`
+        //   ,`<a href="/create">create</a>
+        //   <a href="/update?id=${title}">update</a>
+        //   <form action="delete_process"
+        //         method="post">
+        //     <input type="hidden" name="id" value="${title}">
+        //     <input type="submit" value="delete">
+        //   </form>
+        //   `);
+        //   response.writeHead(200);
+        //   response.end(html);
+        // });
+
+        // });
       }
 
     }else if(pathname=== '/create'){
